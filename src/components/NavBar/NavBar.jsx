ee91bb83d61f41df70277aec6aafd9199b02b3f5
Ahmed Musawir <moose@Ahmeds-Mac.local>
2023-04-21 18:57:21 +0800

    21Apr2023 - last commit until the holidays

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from '@mui/material';
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import useStyles from './styles';
 
-const NavBar = () => <div>NavBar</div>;
+const NavBar = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width: 600px)');
+
+  return (
+    <AppBar positon="fixed">
+      <Toolbar className={classes.toolbar}>
+        {isMobile && (
+          <IconButton
+            color="inherit"
+            edge="start"
+            style={{ outline: 'none' }}
+            onClick={() => {}}
+            className={classes.menuButton}
+          >
+            <Menu />
+          </IconButton>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default NavBar;
---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles(() => ({
+  toolbar: {
+    height: '80px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginLeft: '240px',
+    // background: 'black',
+  },
+}));
